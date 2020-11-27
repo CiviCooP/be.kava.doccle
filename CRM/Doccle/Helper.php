ce020d3693b46f546ee02c96bd86112ec9e8c337
Vincent Polfliet <vinpol@kava.be>
2020-11-27 11:39:13 +0100

    randomness bij secret en batch id

--- CRM/Doccle/Helper.php
+++ CRM/Doccle/Helper.php
@@ -1,10 +1,12 @@
 <?php
 
-class CRM_Doccle_Helper {
+class CRM_Doccle_Helper
+{
 
-  public function createFinancialBox($sourceContactID, $targetContactID) {
-    // get the VAT number of the target contact
-    $sql = "
+    public function createFinancialBox($sourceContactID, $targetContactID)
+    {
+        // get the VAT number of the target contact
+        $sql = "
       select
         btw_nummer_24
       from
@@ -12,21 +14,21 @@ class CRM_Doccle_Helper {
       where
         entity_id = $targetContactID
     ";
-    $vat = CRM_Core_DAO::singleValueQuery($sql);
-    if ($vat) {
-      $doccleName = "Verbinden met Doccle financiële box voor $vat";
-      if (!$this->hasDoccleFields($sourceContactID, $doccleName)) {
-        $this->createDoccleFields($sourceContactID, $doccleName, $vat);
-      }
+        $vat = CRM_Core_DAO::singleValueQuery($sql);
+        if ($vat) {
+            $doccleName = "Verbinden met Doccle financiële box voor $vat";
+            if (!$this->hasDoccleFields($sourceContactID, $doccleName)) {
+                $this->createDoccleFields($sourceContactID, $doccleName, $vat);
+            }
+        } else {
+            throw new Exception("Kan BTW-nummer niet vinden van contact $targetContactID");
+        }
     }
-    else {
-      throw new Exception("Kan BTW-nummer niet vinden van contact $targetContactID");
-    }
-  }
 
-  public function createPatientBox($sourceContactID, $targetContactID) {
-    // get the APB number of the target contact
-    $sql = "
+    public function createPatientBox($sourceContactID, $targetContactID)
+    {
+        // get the APB number of the target contact
+        $sql = "
       select
         apb_nummer_43 apb_nummer
       from
@@ -34,24 +36,24 @@ class CRM_Doccle_Helper {
       where
         entity_id = $targetContactID
     ";
-    $apb = CRM_Core_DAO::singleValueQuery($sql);
-    if ($apb) {
-      $doccleName = "Verbinden met Doccle patientbox voor $apb";
-      if (!$this->hasDoccleFields($sourceContactID, $doccleName)) {
-        $this->createDoccleFields($sourceContactID, $doccleName, $apb);
-      }
+        $apb = CRM_Core_DAO::singleValueQuery($sql);
+        if ($apb) {
+            $doccleName = "Verbinden met Doccle patientbox voor $apb";
+            if (!$this->hasDoccleFields($sourceContactID, $doccleName)) {
+                $this->createDoccleFields($sourceContactID, $doccleName, $apb);
+            }
+        } else {
+            throw new Exception("Kan APB-nummer niet vinden van contact $targetContactID");
+        }
     }
-    else {
-      throw new Exception("Kan APB-nummer niet vinden van contact $targetContactID");
-    }
-  }
 
-  public function hasDoccleFields($contactID, $doccleName) {
-    // see if the contact has a record in the Doccle Connect table
-    $table = CRM_Doccle_Config::singleton()->getDoccleCustomGroup('table_name');
-    $customFieldName = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_label', 'column_name');
+    public function hasDoccleFields($contactID, $doccleName)
+    {
+        // see if the contact has a record in the Doccle Connect table
+        $table = CRM_Doccle_Config::singleton()->getDoccleCustomGroup('table_name');
+        $customFieldName = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_label', 'column_name');
 
-    $sql = "
+        $sql = "
       select
         count(*)
       from
@@ -61,88 +63,110 @@ class CRM_Doccle_Helper {
       and
         $customFieldName = %2
     ";
-    $sqlParams = [
-      1 => [$contactID, 'Integer'],
-      2 => [$doccleName, 'String'],
-    ];
+        $sqlParams = [
+            1 => [$contactID, 'Integer'],
+            2 => [$doccleName, 'String'],
+        ];
 
-    $num = CRM_Core_DAO::singleValueQuery($sql, $sqlParams);
-    if ($num == 0) {
-      return FALSE;
-    }
-    else {
-      return TRUE;
-    }
-  }
-
-  public function createDoccleFields($contactID, $doccleName, $APBorVATnumber) {
-    // get the contact hash
-    $sql = "select hash from civicrm_contact where id  = $contactID";
-    $hash = CRM_Core_DAO::singleValueQuery($sql);
-    if (!$hash) {
-      throw new Exception("Kan hash van contact $contactID niet ophalen.");
+        $num = CRM_Core_DAO::singleValueQuery($sql, $sqlParams);
+        if ($num == 0) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
     }
 
-    // strip BE and spaces from VAT
-    $strippedNumber = $this->stripNumber($APBorVATnumber);
+    public function createDoccleFields($contactID, $doccleName, $receiver)
+    {
+        // get the contact hash
+        $hash = $this->createSecretCode($contactID);
 
-    // create url tokens
-    $token1 = base64_encode($strippedNumber);
-    $token2 = base64_encode($hash);
-    $url = "https://secure.doccle.be/doccle-euui/direct/connect?senderName=kava&t1=$token1&t2=$token2";
+        // create url tokens
+        $token1 = base64_encode($receiver);
+        $token2 = base64_encode($hash);
+        $url = "https://secure.doccle.be/doccle-euui/direct/connect?senderName=kava&t1=$token1&t2=$token2";
 
-    // create the XML
-    $randomID = $contactID . mt_rand(100, 999);
-    $xml = $this->generateXML($randomID, $strippedNumber, $strippedNumber, $hash);
+        // strip BE and spaces from VAT
+        $box = $this->stripNumber($receiver);
 
-    $entityTable = CRM_Doccle_Config::singleton()->getDoccleCustomGroup('table_name');
-    $customFieldLabel = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_label', 'id');
-    $customFieldLink = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_link', 'id');
-    $customFieldReceiverID = CRM_Doccle_Config::singleton()->getDoccleCustomField('Receiver_id', 'id');
-    $customFieldXml = CRM_Doccle_Config::singleton()->getDoccleCustomField('xml', 'id');
+        // create the XML
+        $xml = $this->generateXML($contactID . uniqid(),
+            $receiver,
+            $box,
+            $hash,
+            $url,
+            $token1,
+            $token2
+        );
 
-    $params = [
-      'entity_id' => $contactID,
-      'entity_table' => $entityTable,
-      'custom_' . $customFieldLabel => $doccleName,
-      'custom_' . $customFieldLink => $url,
-      'custom_' . $customFieldReceiverID => $strippedNumber,
-      'custom_' . $customFieldXml => $xml,
-    ];
-    civicrm_api3('CustomValue', 'create', $params);
-  }
+        $entityTable = CRM_Doccle_Config::singleton()->getDoccleCustomGroup('table_name');
+        $customFieldLabel = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_label', 'id');
+        $customFieldLink = CRM_Doccle_Config::singleton()->getDoccleCustomField('Doccle_Connect_link', 'id');
+        $customFieldReceiverID = CRM_Doccle_Config::singleton()->getDoccleCustomField('Receiver_id', 'id');
+        $customFieldXml = CRM_Doccle_Config::singleton()->getDoccleCustomField('xml', 'id');
 
-  private function stripNumber($n) {
-    // remove BE, spaces, and dots
-    return str_replace('BE', '', str_replace('.', '', str_replace(' ', '', $n)));
-  }
+        $params = [
+            'entity_id' => $contactID,
+            'entity_table' => $entityTable,
+            'custom_' . $customFieldLabel => $doccleName,
+            'custom_' . $customFieldLink => $url,
+            'custom_' . $customFieldReceiverID => $box,
+            'custom_' . $customFieldXml => $xml,
+        ];
+        civicrm_api3('CustomValue', 'create', $params);
+    }
 
-  private function generateXML($batchID, $receiverID, $boxID, $uuID) {
-    $startDate = new DateTime();
-    $endDate = new DateTime();
-    $endDate->add(new DateInterval('P6M'));
+    private function createSecretCode($contact)
+    {
+        $sql = "select hash from civicrm_contact where id  = $contact";
+        $hash = CRM_Core_DAO::singleValueQuery($sql);
 
-    $xml =
-'<?xml version="1.0" encoding="utf-8"?>
+        if (!$hash) {
+            throw new Exception("Kan hash van contact '$contact' niet ophalen.");
+        }
+
+        // add randomness to hash so that links can be generated for the same contact
+        return $hash . uniqid();
+    }
+
+    private function stripNumber($n)
+    {
+        // remove BE, spaces, and dots
+        return str_replace('BE', '', str_replace('.', '', str_replace(' ', '', $n)));
+    }
+
+    private function generateXML($batchID, $receiver, $box, $uuid, $url, $token1, $token2)
+    {
+        $startDate = new DateTime();
+        $endDate = new DateTime();
+        $endDate->add(new DateInterval('P6M'));
+
+        $escapedUrl = htmlspecialchars($url);
+
+        $xml =
+            '<?xml version="1.0" encoding="utf-8"?>
  <batch xmlns="http://ixor.be/docs/receivers">
     <batch-info>
         <id>receiverbatch_' . $batchID . '</id>
     </batch-info>
     <receivers>
         <receiver>
-            <id>' . $receiverID . '</id>
+            <!-- ' . $escapedUrl . ' -->
+            <id>' . $box . '</id>
             <token-set start-date=\'' . $startDate->format('Y-m-d\TH:i:s') . '\' end-date=\'' . $endDate->format('Y-m-d\TH:i:s') . '\' >
                 <token>
-                    <value>'. $boxID . '</value>
+                    <!-- token1: ' . $token1 . ' -->
+                    <value>' . $receiver . '</value>
                 </token>
                 <token>
-                    <value>' . $uuID . '</value>
+                    <!-- token2: ' . $token2 . ' --> 
+                    <value>' . $uuid . '</value>
                 </token>
             </token-set>
         </receiver>
     </receivers>
 </batch>';
 
-    return $xml;
-  }
+        return $xml;
+    }
 }
